98ce91feb9959891f157c30000b93ca52f91e41b
Anthony Boutinov <anton4488@gmail.com>
2021-07-25 22:30:11 +0300

    Initial Commit

---
+++ PersonalMobilityTravelTime/PersonalMobilityTravelTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalMobilityTravelTimeApp.swift
+//  PersonalMobilityTravelTime
+//
+//  Created by Anthony Boutinov on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct PersonalMobilityTravelTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
